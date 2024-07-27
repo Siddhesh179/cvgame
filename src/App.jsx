4c6b58af15b4fb0660ec939885d193eb7322f172
Siddhesh179 <siddhesh8@outlook.com>
2024-07-27 11:52:46 +0530

    Commiting

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./Home";
+import Header from "./Header";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,32 @@
+import styles from "./home.module.css";
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+      <div className={styles.boxes}></div>
+    </div>
+  );
+}
